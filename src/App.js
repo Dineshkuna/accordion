f938a2e4b781034916012baf55597a008440c14a
DineshKuna <kunadinesh.work@gmail.com>
2024-09-08 13:27:51 +0530

    version1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 
 import './App.css';
+import Rating from './components/star_rating/Starrating';
 // import Accordion from './components/accordion/Accordion';
-import RandomColor from './components/randomColor/RandomColor';
+// import RandomColor from './components/randomColor/RandomColor';
 
 function App() {
   return (
     <div className="App">
       {/* <Accordion/> */}
-      <RandomColor/>
+      {/* <RandomColor/> */}
+      <Rating/>
       
     </div>
   );
---
+++ src/components/star_rating/Starrating.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { FaStar } from 'react-icons/fa';
+
+
+
+const Rating = ({onStars = 5}) => {
+
+    const [rating ,setRating] = useState(0)
+    const [hover ,setHover] = useState(0)
+
+
+    const handleClick = (index) => {
+        setRating(index + 1);
+    }
+
+    const handleMove = (index) => {
+        setHover(index + 1);
+    }
+
+    const handleOver = (index) => {
+        setHover(index + 1);
+    }
+
+
+    return (
+        <div>
+            {[...Array(onStars)].map((_, index) => (
+            <FaStar
+                key = {index}
+                onClick={() => handleClick(index)}
+                onMouseMove={() => handleMove(index)}
+                onMouseOver={() => handleOver (index)}
+                color={index + 1 <= (hover || rating) ? 'yellow' : 'black'}                                        
+            />
+        ))}
+        </div>
+        )
+    }
+
+export default Rating;
