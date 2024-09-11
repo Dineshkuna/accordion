cc0f080a54d0d7c332af92f82ac4a5802012a594
DineshKuna <kunadinesh.work@gmail.com>
2024-09-11 13:15:59 +0530

    version2

--- src/components/load-data/LoadData.jsx
+++ src/components/load-data/LoadData.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
 
 const LoadMoreData = () => {
-    return (
-        <div>
-            Hi
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchdata = async () => {
+      try {
+        const response = await fetch(
+          "https://dummyjson.com/products?limit=10&skip=10&select=title,price"
+        );
+        const result = await response.json();
+        console.log(result);
+        setData(result.products);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchdata();
+  }, []);
+
+  return (
+    <div>
+      {data.map((item, index) => (
+        <div key={index}>
+          <p>{item.title}</p>
+          <p>{item.price}</p>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 export default LoadMoreData;
