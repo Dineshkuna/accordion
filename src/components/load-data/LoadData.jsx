7a7d8d9f678c26e5cbe0ad2819e1324f4aaa0cc0
DineshKuna <kunadinesh.work@gmail.com>
2024-09-17 09:54:53 +0530

    version3

--- src/components/load-data/LoadData.jsx
+++ src/components/load-data/LoadData.jsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 
 const LoadMoreData = () => {
   const [data, setData] = useState([]);
+  const [count, setCount] = useState(0)
   useEffect(() => {
     const fetchdata = async () => {
       try {
         const response = await fetch(
-          "https://dummyjson.com/products?limit=10&skip=10&select=title,price"
+          
+        `https://dummyjson.com/products?limit=100&skip=${
+          count === 0 ? 0 : count * 20
+        }`
         );
         const result = await response.json();
         console.log(result);
@@ -16,16 +20,34 @@ const LoadMoreData = () => {
       }
     };
     fetchdata();
-  }, []);
+  }, [count]);
 
   return (
-    <div>
+    <div style={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      gap:'10px',
+      padding: '10px 10px 10px 30px'
+      
+    }}>
       {data.map((item, index) => (
-        <div key={index}>
+        <div style={{
+          width: '350px',
+          height: '350px',
+          border: 'solid 1px',
+          
+        }} key={index}>
           <p>{item.title}</p>
           <p>{item.price}</p>
+          
         </div>
       ))}
+      <button style={{
+        padding : '10px',
+        
+        
+        
+      }}>LoadMore</button>
     </div>
   );
 };
