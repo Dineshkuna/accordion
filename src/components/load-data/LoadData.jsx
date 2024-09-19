e286a6b7f72fa60a10cd1fe2104ed17e9ee3cc76
DineshKuna <kunadinesh.work@gmail.com>
2024-09-19 09:07:07 +0530

    version5

--- src/components/load-data/LoadData.jsx
+++ src/components/load-data/LoadData.jsx
@@ -54,6 +54,7 @@ const LoadMoreData = () => {
         }} key={index}>
           <p>{item.title}</p>
           <p>{item.price}</p>
+          <img src={`https://images.unsplash.com/photo-1492707892479-7bc8d5a4ee93?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D${index + 1}`} alt="productImg" width={300} height={250}/>
           
         </div>
       ))}
@@ -65,6 +66,7 @@ const LoadMoreData = () => {
       >
         {loading ? 'Loading...' : 'Load More'}
       </button>
+      
     </div>
   );
 };
