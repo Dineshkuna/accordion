772f7bab6252260b0f81ddb9f9c227bca4bd13be
DineshKuna <kunadinesh.work@gmail.com>
2024-09-19 07:04:46 +0530

    version4

--- src/components/load-data/LoadData.jsx
+++ src/components/load-data/LoadData.jsx
@@ -2,26 +2,41 @@ import React, { useEffect, useState } from "react";
 
 const LoadMoreData = () => {
   const [data, setData] = useState([]);
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [hasMore, setHasMore] = useState(true);
+
   useEffect(() => {
     const fetchdata = async () => {
+      if (count < 5) { 
+        setLoading(true);
       try {
         const response = await fetch(
           
-        `https://dummyjson.com/products?limit=100&skip=${
-          count === 0 ? 0 : count * 20
-        }`
+        `https://dummyjson.com/products?limit=20&skip=${
+          count * 20}`
         );
         const result = await response.json();
-        console.log(result);
-        setData(result.products);
+        console.log( result );
+
+        
+        setData((prevData)=>[...prevData , ...result.products]);
+        setHasMore(result.products.length === 20);
       } catch (error) {
         console.log(error);
       }
+      setLoading(false);
+    }
     };
     fetchdata();
   }, [count]);
 
+  const loadMore = () => {
+    if (hasMore) {
+      setCount((prevCount) => prevCount + 1);
+    }
+  };
+  
   return (
     <div style={{
       display: 'flex',
@@ -42,12 +57,14 @@ const LoadMoreData = () => {
           
         </div>
       ))}
-      <button style={{
-        padding : '10px',
-        
-        
-        
-      }}>LoadMore</button>
+      <button 
+      onClick={loadMore}
+      style={{ padding: '10px' }}
+      disabled={!hasMore || loading}
+      
+      >
+        {loading ? 'Loading...' : 'Load More'}
+      </button>
     </div>
   );
 };
