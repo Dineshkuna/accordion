29632a684eff5f04d75e006438b91e5cacfe724b
DineshKuna <kunadinesh.work@gmail.com>
2024-10-03 12:09:11 +0530

    version2

--- src/components/tree-view/TreeMenu.jsx
+++ src/components/tree-view/TreeMenu.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Menus from "./data";
 
 const TreeMenu = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  function handleChild(index) {
+    setActiveIndex(index);
+  }
+
   return (
     <div>
-      {Menus.map((item, index) => <p key={index}> {item.label}</p>)}
+      <div>
+        {Menus.map((item, index) => (
+          <div key={index}>
+            <p onClick={() => handleChild(index)}>
+              
+              <span>{item.label} +</span>
+            </p>
+
+            {activeIndex === index && item.children && (
+              <div>
+                {item.children.map((child, childIndex) => (
+                  <p key={childIndex}>{child.label}</p>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
